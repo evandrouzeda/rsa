c4615450438195800745118503be6b518f1a8c1e
evandrouzeda <evandrouzeda@gmail.com>
2022-01-21 15:25:36 +0000

    dividindo em funcoes

--- usecase/generateKeysFromPublic.py
+++ usecase/generateKeysFromPublic.py
@@ -1,4 +1,4 @@
-from usecase.createKeys import xgcd
+from usecase.createKeys import xgcd, getD
 from entity.crypto import Crypto
 
 class GenerateKeysFromPublic:
@@ -7,11 +7,6 @@ class GenerateKeysFromPublic:
 
     def execute(p, q, e, n):
         t = (p-1)*(q-1)
-
-        g, x, y = xgcd(e, t)
-        if(x < 0):
-            d = x + t
-        else: 
-            d = x
+        d = getD(e, t)
 
         return Crypto(n, e, d)
